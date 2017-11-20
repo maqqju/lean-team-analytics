f239714f87bf0f9b44ccca0f2b1ac112e3338c34
Matthew Croker <matthew.croker@ixaris.com>
2017-11-20 14:07:32 +0100

    fixed documentation

--- reporting-server/server.js
+++ reporting-server/server.js
@@ -11,45 +11,45 @@ function ReportingServer(config) {
 	});
 
 	/**
-	 * A rest API that returns three-point statistical data on cycle time
+	 * A REST API that returns three-point statistical data on cycle time
 	 */
 	app.get("/three-pt-data/cycle", (req,res) => {
-		res.json({message : "A rest API that returns three-point statistical data on cycle time"});
+		res.json({message : "A REST API that returns three-point statistical data on cycle time"});
 	});
 
 	/**
-	 * A rest API that returns three-point statistical data on stories
+	 * A REST API that returns three-point statistical data on stories
 	 */
 	app.get("/three-pt-data/stories", (req,res) => {
-		res.json({message : "A rest API that returns three-point statistical data on stories"});
+		res.json({message : "A REST API that returns three-point statistical data on stories"});
 	});
 
 	/**
-	 * A rest API that returns historical data on phases
+	 * A REST API that returns historical data on phases
 	 */
 	app.get("/data/stories", (req,res) => {
-		res.json({message : "A rest API that returns historical data on phases"});
+		res.json({message : "A REST API that returns historical data on phases"});
 	});
 
 	/**
-	 * A rest API that returns historical data on stories
+	 * A REST API that returns historical data on stories
 	 */
 	app.get("/data/stories", (req,res) => {
-		res.json({message : "A rest API that returns historical data on stories"});
+		res.json({message : "A REST API that returns historical data on stories"});
 	});
 
 	/**
-	 * A rest API that returns historical data on stories
+	 * A REST API that returns historical data on stories
 	 */
 	app.get("/sprint/data", (req,res) => {
-		res.json({message : "A rest API that forwards sprint-data from JIRA"});
+		res.json({message : "A REST API that forwards sprint-data from JIRA"});
 	});
 
 	/**
-	 * A rest API that forwards sprint task-data from JIRA
+	 * A REST API that forwards sprint task-data from JIRA
 	 */
 	app.get("/sprint/tasks", (req,res) => {
-		res.json({message : "A rest API that forwards sprint task-data from JIRA"});
+		res.json({message : "A REST API that forwards sprint task-data from JIRA"});
 	});
 }
 
