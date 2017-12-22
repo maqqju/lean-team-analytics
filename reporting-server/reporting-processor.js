34d545432fcd1881447145ca21715dc95341b15b
Matthew Croker <matthew.croker@ixaris.com>
2017-12-22 11:33:50 +0100

    Solved issue on story points, now to focus on cycle-time

--- reporting-server/reporting-processor.js
+++ reporting-server/reporting-processor.js
@@ -16,68 +16,52 @@ const STORY_POINTS = [1,2,3,5,8,13,21];
  * @param  {[type]}
  * @return {[type]}
  */
-function removeNoise(rawChangeList) {
-	let changeList = rawChangeList.map((time) => Number(time)).sort((a,b) => a - b).filter((time) => time > 0).sort((a,b) => a-b);
-	console.log("Filter 1");
-	let processed = [].concat(changeList)
-					 .map((timespent) => Math.log10(timespent))
-					 .map((log, index, list) => (index > 0 ? log / list[index-1] : 0) % 1);
+function removeNoise(rawChangeList, thresholdPoint) {
+	return new Promise((resolve) => {
+		let changeList = rawChangeList.map((time) => Number(time)).sort((a,b) => a - b).filter((time) => time > 0).sort((a,b) => a-b);
+		let processed = [].concat(changeList)
+						 .map((timespent) => Math.log10(timespent))
+						 .map((log, index, list) => (index > 0 ? log / list[index-1] : 0) % 1);
 
-	console.log("Filter 2");
-	let processedForThreshold = [].concat(processed).sort((dec1,dec2) => dec1 - dec2);
-	
-	console.log("Filter 3");
-	let minThreshIndex = Math.floor(processedForThreshold.length * 0.95);
-	let maxThreshIndex = Math.floor(processedForThreshold.length * 0.97);
+		let processedForThreshold = [].concat(processed).sort((dec1,dec2) => dec1 - dec2);
+		let threshold = processedForThreshold[Math.floor(processedForThreshold.length * 0.97)];
 
-	console.log("Filter 4");
+		/**
+		 * here we are choosing the index points highlighted by outliers
+		 * e.g. [1,3,100,105], and the threshold for difference is 5, then 100 is marked as an outlier
+		 * so this process will give us a list with an outlier [2]
+		 * additionally we are adding the index 0, and the end of list
+		 * so that we can choose between the segments 0..2,2..3, and choose the biggest segment.
+		 * 
+		 * Marked points will be excluded.
+		 */
+		let maxSegmentsPoints = processed.map((item, index) => { return { item : item, index : index}}).filter((item) => item.item >= maxThresh)
+										 .map((item) => item.index);
 
-	let minThresh = processedForThreshold[minThreshIndex];
-	let maxThresh = processedForThreshold[maxThreshIndex];
+		/**
+		 * Transforms a list of points to segments to the original list
+		 * 
+		 * @param  {[type]} segmentPoints [description]
+		 * @return {[type]}               [description]
+		 */
+		let transformToSegments = (segmentPoints) => new Promise((resolve) => {
+			resolve(segmentPoints.map((point, index, list) => {
+				if (index === 0) {
+					return {start : 0, end : point, size : point}
+				} else if (index === list.length-1) {
+					return {start : point, end : processed.length, size : (processed.length-1) - (point)}
+				} else {
+					return {start : list[index-1], end : point, size : point-list[index-1]}
+				}
+			}).sort((a,b) => b.size - a.size));
+		});
 
+		let max = transformToSegments(maxSegmentsPoints).then((maxSeg) => {
+			return changeList.filter((change, index) => maxSeg[0] && (index > maxSeg[0].start || index < maxSeg[0].end));
+		});
 
-
-	// here we are choosing the index points highlighted by outliers
-	// e.g. [1,3,100,105], and the threshold for difference is 5, then 100 is marked as an outlier
-	// so this process will give us a list with an outlier [2]
-	// additionally we are adding the index 0, and the end of list
-	// so that we can choose between the segments 0..2,2..3, and choose the biggest segment.
-	// 
-	// Marked points will be excluded.
-	
-	let minSegmentsPoints = processed.map((item, index) => { return { item : item, index : index}}).filter((item) => item.item >= minThresh)
-									 .map((item) => item.index);
- 	console.log("Filter 5");
-	let maxSegmentsPoints = processed.map((item, index) => { return { item : item, index : index}}).filter((item) => item.item >= maxThresh)
-									 .map((item) => item.index);
-
-	console.log("Filter 6");
-	/**
-	 * Transforms a list of points to segments to the original list
-	 * 
-	 * @param  {[type]} segmentPoints [description]
-	 * @return {[type]}               [description]
-	 */
-	let transformToSegments = (segmentPoints) => {
-		segmentPoints.map((point, index, list) => {
-			if (index === 0) {
-				return {start : 0, end : point, size : point}
-			} else if (index === list.length-1) {
-				return {start : point, end : processed.length, size : (processed.length-1) - (point)}
-			} else {
-				return {start : list[index-1], end : point, size : point-list[index-1]}
-			}
-		}).sort((a,b) => b.size - a.size);
-	}
-	let minSeg = transformToSegments(minSegmentsPoints);
-	console.log("Filter 7");
-	let maxSeg = transformToSegments(maxSegmentsPoints);
-	console.log("Filter 8");
-
-	return {
-		min : changeList.filter((change, index) => minSeg[0] && (index >= minSeg[0].start || index <= minSeg[0].end)),//processed.map((item, index) => { return { item : item, index : index}}).filter((item) => item.item <= minThresh).map((item) => changelist[item.index]),
-		max : changeList.filter((change, index) => maxSeg[0] && (index >= maxSeg[0].start || index <= maxSeg[0].end))
-	}
+		resolve(max);
+	});
 }
 
 
@@ -88,12 +72,8 @@ function removeNoise(rawChangeList) {
  * @return {[type]}
  */
 function getThreePointValuesFromList(list, sp) {
-	return new Promise((resolve) => {
-		sp && console.log("Cleaning data for ", sp);
-		//list.map((time) => Number(time)).sort((a,b) => a - b).filter((time) => time > 0)
-		Promise.resolve(removeNoise(list)).then((cleanedListPayload) => {
+	return removeNoise(list, 0.95).then((sortedList) => new Promise((resolve) => {
 			sp && console.log("Data cleaned for ", sp);
-			let sortedList = cleanedListPayload.max;
 			let bestCase = sortedList[0];
 			let worstCase = sortedList[sortedList.length-1];
 			let mostLikely = sortedList.reduce((acc,value) => acc + value, 0) / sortedList.length;
@@ -103,9 +83,9 @@ function getThreePointValuesFromList(list, sp) {
 			let weightedaverage = (bestCase + worstCase + (4*mostLikely)) / 6;
 
 			resolve({ sd : sd, weightedaverage : weightedaverage });
-		});
-	})
+		}));
 }
+	 
 
 /**
  * Crunches a given value and inserts it to DB
